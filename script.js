db98f0f997efa3474a3a94366ea3870775d7e98b
mjlanoy05-prog <mjlanoy05@gmail.com>
2026-08-04 01:11:50 +0800

    Update script.js
    Adding Day 2 - Day 17. Test 1

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbxvMZJ7aqYOj3mWzO2arKOWdmP6r2LNPEwHOPWpJlh4CrBeMBRlqJ9XhzjkalcKXL89/exec";
+"https://script.google.com/macros/s/AKfycbw1-BpQzSbBRSzOgJ-yHvYlAz6T4Vt9VpnpBweWe8kKWvu8WmkuDokQzQqMsW5Uxj5G/exec";
 
 
 
-
-
-// ======================
-// ADMIN LOGIN
-// ======================
 
 
 const ADMIN_USERNAME = "admin";
@@ -16,25 +11,33 @@ const ADMIN_PASSWORD = "12345";
 
 
 
+let currentQR="";
+
+let scanner;
+
+
+
+
+
+
 function login(){
 
 
-
-let username =
+let user =
 document.getElementById("username").value;
 
 
 
-let password =
+let pass =
 document.getElementById("password").value;
 
 
 
 
-if(
-username === ADMIN_USERNAME &&
-password === ADMIN_PASSWORD
 
+if(
+user==ADMIN_USERNAME &&
+pass==ADMIN_PASSWORD
 ){
 
 
@@ -43,7 +46,6 @@ document.getElementById("login").style.display="none";
 
 
 document.getElementById("attendance").style.display="block";
-
 
 
 startScanner();
@@ -55,15 +57,17 @@ startScanner();
 else{
 
 
-document.getElementById("loginStatus").innerHTML =
-"Invalid Username or Password";
+document.getElementById("loginStatus").innerHTML=
+"Invalid Login";
+
+
+}
 
 
 }
 
 
 
-}
 
 
 
@@ -80,122 +84,6 @@ location.reload();
 
 
 
-
-
-
-
-// ======================
-// QR ATTENDANCE
-// ======================
-
-
-
-let currentQR = "";
-
-let scanner;
-
-
-
-
-
-// QR SCANNED
-
-
-function qrSuccess(decodedText){
-
-
-
-currentQR =
-decodedText.trim();
-
-
-
-document
-.getElementById("status")
-.innerHTML =
-"Detected: "
-+
-currentQR;
-
-
-
-sendAttendance();
-
-
-
-}
-
-
-
-
-
-
-
-
-// SEND QR TO APPS SCRIPT
-
-
-function sendAttendance(){
-
-
-
-fetch(API_URL,{
-
-method:"POST",
-
-body:JSON.stringify({
-
-qr_id:currentQR
-
-})
-
-})
-
-
-.then(response=>response.json())
-
-
-.then(data=>{
-
-
-document
-.getElementById("result")
-.innerHTML =
-
-
-`
-
-<h2>${data.message}</h2>
-
-
-<p>
-${data.name || ""}
-</p>
-
-
-<p>
-${data.course || ""}
-</p>
-
-
-`;
-
-
-
-});
-
-
-
-}
-
-
-
-
-
-
-
-
-// START CAMERA AFTER LOGIN
 
 
 function startScanner(){
@@ -222,6 +110,30 @@ qrbox:300
 scanner.render(qrSuccess);
 
 
+}
+
+
+
+
+
+
+function qrSuccess(decodedText){
+
+
+
+currentQR =
+decodedText.trim();
+
+
+
+document.getElementById("status").innerHTML=
+"Detected: "+currentQR;
+
+
+
+sendAttendance();
+
+
 
 }
 
@@ -231,8 +143,66 @@ scanner.render(qrSuccess);
 
 
 
+function sendAttendance(){
 
-// CHECK ATTENDANCE BUTTON
+
+
+let dayColumn =
+document.getElementById("daySelect").value;
+
+
+
+
+
+fetch(API_URL,{
+
+method:"POST",
+
+body:JSON.stringify({
+
+qr_id:currentQR,
+
+attendance_column:dayColumn
+
+
+})
+
+})
+
+
+
+.then(response=>response.json())
+
+
+
+.then(data=>{
+
+
+
+document.getElementById("result").innerHTML=
+
+
+`
+
+<h2>${data.message}</h2>
+
+<p>${data.name || ""}</p>
+
+<p>${data.course || ""}</p>
+
+`;
+
+
+});
+
+
+
+}
+
+
+
+
+
 
 
 function checkAttendance(){
@@ -242,15 +212,10 @@ function checkAttendance(){
 if(currentQR==""){
 
 
-
-alert(
-"Please scan QR first"
-);
-
+alert("Please scan QR first");
 
 
 return;
-
 
 
 }
@@ -258,47 +223,66 @@ return;
 
 
 
+
 fetch(
-
 API_URL+
-"?action=check&qr_id="
-+
-currentQR
-
+"?action=check&qr_id="+currentQR
 )
 
 
+
 .then(response=>response.json())
+
 
 
 .then(data=>{
 
 
 
-document
-.getElementById("result")
-.innerHTML =
+document.getElementById("result").innerHTML=
 
 
 `
 
-<h2>
-${data.name}
-</h2>
+<h2>${data.name}</h2>
 
-
-<p>
-${data.course}
-</p>
-
+<p>${data.course}</p>
 
 <hr>
 
+<p>DAY 1: ${data.day1}</p>
 
-<p>
-DAY 1:
-${data.day1}
-</p>
+<p>DAY 2: ${data.day2}</p>
+
+<p>DAY 3: ${data.day3}</p>
+
+<p>DAY 4: ${data.day4}</p>
+
+<p>DAY 5: ${data.day5}</p>
+
+<p>DAY 6: ${data.day6}</p>
+
+<p>DAY 7: ${data.day7}</p>
+
+<p>DAY 8: ${data.day8}</p>
+
+<p>DAY 9: ${data.day9}</p>
+
+<p>DAY 10: ${data.day10}</p>
+
+<p>DAY 11: ${data.day11}</p>
+
+<p>DAY 12: ${data.day12}</p>
+
+<p>DAY 13: ${data.day13}</p>
+
+<p>DAY 14: ${data.day14}</p>
+
+<p>DAY 15: ${data.day15}</p>
+
+<p>DAY 16: ${data.day16}</p>
+
+<p>DAY 17: ${data.day17}</p>
 
 
 `;
